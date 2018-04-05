a73aefcdd8b480926ccc48d7b5fda4cbdc790f8d
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2018-04-05 15:42:34 +0530

    Changes to make the local server https.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,9 +1,13 @@
-$.ajax({
-  url: 'http://localhost:3131/'+window.location.hostname.replace(/^www\./, '')+'.js',
-  success: function(d){
-    $(function(){ eval(d); });
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131');
-  }
+$(function() {
+    var protocol = (window.location.protocol === "https:") ? "https:" : "http:";
+    protocol = "https:";
+    $.ajax({
+	      url: protocol + '//localhost:3131/'+window.location.hostname.replace(/^www\./, '')+'.js',
+	      success: function(d){
+	          $(function(){ eval(d); });
+	      },
+	      error: function(){
+	          console.log('no dotjs server found at localhost:3131');
+	      }
+    });
 });
